db3d065a6c454cb04db8298074c5eaec30e80aaa
Ahmed Afri <ahamedafricontact@gmail.com>
2023-08-20 23:18:14 +0530

    homepage completed

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom"
+import "./menu.scss";
+import {menu} from "../../data";
 
 const Menu = () => {
   return (
-    <div>menu</div>
-  )
+        <div className="menu">
+       
+            {menu.map(item => ( 
+                <div className="item" key={item.id}>
+                  <div className="title">{item.title}</div>
+                    
+
+                      {item.listItems.map(listItem => ( <Link to="/" className="listItem" key={listItem.id}>
+                        <img src={listItem.icon} alt=""/>
+                        <span className="ListItemTitle">{listItem.title}</span>
+                      </Link>
+                      ))}
+                      
+                </div>
+            ))}
+
+        </div>
+)
 }
 
 export default Menu
